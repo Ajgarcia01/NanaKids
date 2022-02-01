2f7dde81e31f35248b1b5233ef9c7a7903f6f2e1
zalobmq <gbretonesmoraquero@gmail.com>
2022-02-01 14:34:55 +0100

    gfelicitation

--- src/main/java/com/app/ApiRestFul/controller/FelicitationController.java
+++ src/main/java/com/app/ApiRestFul/controller/FelicitationController.java
@@ -1,9 +1,22 @@
 package com.app.ApiRestFul.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.ApiRestFul.exceptions.RecordNotFoundException;
+import com.app.ApiRestFul.model.Felicitation;
 import com.app.ApiRestFul.services.FelicitationService;
 
 @RestController
@@ -12,5 +25,81 @@ public class FelicitationController {
 
 	@Autowired
 	FelicitationService service;
-	//antes de tocar esto tener el servicio hecho
+	// antes de tocar esto tener el servicio hecho
+	
+	//-----------------GET
+
+	@GetMapping
+	public ResponseEntity<List<Felicitation>> getAllFelicitations() {
+		List<Felicitation> list = service.getAllFelicitations();
+		return new ResponseEntity<List<Felicitation>>(list, new HttpHeaders(), HttpStatus.OK);
+	}
+	//Get felicitation by ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Felicitation> getItemById(@PathVariable("id") Long id) throws RecordNotFoundException {
+		
+		Felicitation entity = service.getFelicitationById(id);
+
+		return new ResponseEntity<Felicitation>(entity, new HttpHeaders(), HttpStatus.OK);
+	}
+	
+	//Get count felicitations
+	@GetMapping("/count/")
+	public Long numberFelicitations() {
+		Long result = service.numberFelicitations();
+		return result;
+	}
+	
+	//Get ListFelicitation by type
+	@GetMapping("/search/{type}")
+	public ResponseEntity<List<Felicitation>> getListByType( @PathVariable("type")int type){
+		List<Felicitation> list  =  service.listTypeFelicitation(type);
+		return new ResponseEntity<List<Felicitation>>(list , new HttpHeaders() , HttpStatus.OK);
+	}
+	
+	//Get felicitation by id
+	
+	//-----------------------------------POST
+	
+	 @PostMapping
+	    public ResponseEntity<Felicitation> createOrUpdateItem(@Validated @RequestBody Felicitation felicitation) throws RecordNotFoundException {
+		 Felicitation updated = service.createOrUpdate(felicitation);
+	        return new ResponseEntity<Felicitation>(updated, new HttpHeaders(), HttpStatus.OK);
+	}
+	 
+	 
+	 //CAMBIAR ESTADO DE LA LISTA A ENVIADO
+	 /*DEVOLVIENDO UNA LISTA 
+	
+	@PostMapping
+	public ResponseEntity<List<Felicitation>> updateStatus(){
+		
+		List<Felicitation> list = service.getAllFelicitations();
+		List<Felicitation> ls =   service.changeStatusFelicitation(list);
+		
+		return new ResponseEntity<List<Felicitation>>(ls , new HttpHeaders() , HttpStatus.OK);
+		
+	}
+	*/
+	 //CAMBIAR ESTADO DE LA LISTA A ENVIADO
+	 //DEVOLVIENDO UN HttpStatus
+	 @PostMapping
+		public HttpStatus updateStatus(){
+		 List<Felicitation> list = service.getAllFelicitations();
+		 service.changeStatusFelicitation(list);
+		 return HttpStatus.OK;
+	 }
+	 
+	//-----------------------DELETE
+	 @DeleteMapping("/{id}")
+	    public HttpStatus deleteItemById(@PathVariable("id") Long id) throws RecordNotFoundException {
+	        service.deleteFelicitation(id);
+	        return HttpStatus.OK;
+	    }
+	
+	
+
+	
+	
+
 }
--- src/main/java/com/app/ApiRestFul/services/FelicitationService.java
+++ src/main/java/com/app/ApiRestFul/services/FelicitationService.java
@@ -1,52 +1,137 @@
 package com.app.ApiRestFul.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.app.ApiRestFul.exceptions.RecordNotFoundException;
 import com.app.ApiRestFul.model.Felicitation;
 import com.app.ApiRestFul.repository.FelicitationRepository;
 
-@Service
 public class FelicitationService {
 	@Autowired
 	FelicitationRepository repository;
-	//OBTENER TODAS LAS FELICITACIOS
-		public List<Felicitation> getAllFelicitations(){
-			List<Felicitation> result = repository.findAll();
-			return result;
+
+	// OBTENER TODAS LAS FELICITACIOS
+	public List<Felicitation> getAllFelicitations() {
+		List<Felicitation> result = repository.findAll();
+		return result;
+	}
+
+	// OBTENER FELICITACIONES POR ID
+	public Felicitation getFelicitationById(Long id) throws RecordNotFoundException {
+		Optional<Felicitation> result = repository.findById(id);
+		if (result.isPresent()) {
+			return result.get();
+
+		} else {
+			throw new RecordNotFoundException("", id);
 		}
-		//OBTENER FELICITACIONES POR ID 
-		public Felicitation getFelicitationById(Long id){
-			Optional<Felicitation> result = repository.findById(id);
-			if (result.isPresent()) {
-				return result.get();
-				
-			}else {	
-				throw new RecordNotFoundException("", id);
+
+	}
+
+	// CREAR O ACTUALIZAR FELICITACION
+	public Felicitation createOrUpdate(Felicitation felicitation) {
+		if (felicitation.getId() != null && felicitation.getId() > 0) {
+			Optional<Felicitation> f = repository.findById(felicitation.getId());
+
+			if (f.isPresent()) {
+
+				// Actualizar
+
+				Felicitation newFelicitation = f.get();
+				newFelicitation.setId(felicitation.getId());
+				newFelicitation.setImage(felicitation.getImage());
+				return newFelicitation;
+			} else {
+				felicitation.setId(null);
+				felicitation = repository.save(felicitation);
+				return felicitation;
 			}
-			
+		} else {
+			felicitation = repository.save(felicitation);
+			return felicitation;
 		}
-		//CREAR O ACTUALIZAR FELICITACION
-		
-		/*
-		public Felicitation createOrUpdate(Felicitation felicitation) {
-			if ()
+	}
+
+	// BORRAR FELICITACION POR ID
+	public void deleteFelicitation(Long id) {
+
+		Optional<Felicitation> result = repository.findById(id);
+		if (result.isPresent()) {
+			repository.deleteById(id);
+		} else {
+			// NOTA BORRADA POR ID
 		}
-		*/
-		
-		//BORRAR FELICITACION POR ID
-		public void deleteFelicitation(Long id) {
-			
-			Optional<Felicitation> result  = repository.findById(id);
-			if(result.isPresent()) {
-				repository.deleteById(id);
-			}else {
-				//NOTA BORRADA POR ID
+	}
+	/*
+	 * public int numberFelicitation() { List<Felicitation> result =
+	 * repository.findAll(); int number = result.size(); return number;
+	 * 
+	 * }
+	 */
+	// OBTENER NUMERO DE FELICITACIONES
+
+	public Long numberFelicitations() {
+		Long number = repository.count();
+		return number;
+
+	}
+	// CAMBIAR ESTADO DE FELICITACION
+	// DEVUELVE TRUE SI TODAS CAMBIAN SU ESTADO A ENVIADO
+	/*
+	 * public boolean changeStatusFelicitation(List<Felicitation> listFelicitation)
+	 * {
+	 * 
+	 * Felicitation x; boolean result = false;
+	 * 
+	 * if (listFelicitation != null && listFelicitation.size() > 0) { for(int
+	 * i=0;i<listFelicitation.size();i++) {
+	 * 
+	 * x = listFelicitation.get(i); if(!x.isEstate()) { x.setEstate(true); }
+	 * 
+	 * } result = true;
+	 * 
+	 * } return result; }
+	 */
+	/*
+	 * //????DUDA????? public List<Felicitation>
+	 * changeStatusFelicitation(List<Felicitation> listFelicitation) {
+	 * 
+	 * 
+	 * Felicitation x; boolean status = true;
+	 * 
+	 * if (listFelicitation != null && listFelicitation.size() > 0) {
+	 * 
+	 * for(int i=0;i<listFelicitation.size();i++) { x = listFelicitation.get(i);
+	 * if(!x.isEstate()) { x = repository.updateStatus(status, x.getId()); } }
+	 * 
+	 * } List<Felicitation> ls = repository.findAll(); return ls;
+	 * 
+	 * }
+	 */
+
+	//DEVUELVE UN HTTPSTATUS
+	public void changeStatusFelicitation(List<Felicitation> listFelicitation) {
+		Felicitation x;
+		boolean status = true;
+		if (listFelicitation != null && listFelicitation.size() > 0) {
+
+			for (int i = 0; i < listFelicitation.size(); i++) {
+				x = listFelicitation.get(i);
+				if (!x.isEstate()) {
+					x = repository.updateStatus(status, x.getId());
+				}
 			}
+
 		}
-		
+	}
+
+	public List<Felicitation> listTypeFelicitation(int TypeFelicitation) {
+		List<Felicitation> listFelicitation = repository.getByType(TypeFelicitation);
+		return listFelicitation;
+	}
+
 }
