69d8a8a1b0874e29f6a02eb45d50e400ddd3a88d
edu-cl123 <educar200@gmail.com>
2022-02-10 11:00:18 +0100

    mi rama

--- src/main/java/com/app/ApiRestFul/services/ClientService.java
+++ src/main/java/com/app/ApiRestFul/services/ClientService.java
@@ -2,6 +2,9 @@ package com.app.ApiRestFul.services;
 
 import com.app.ApiRestFul.exceptions.RecordNotFoundException;
 import com.app.ApiRestFul.model.Client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,80 +15,159 @@ import java.util.Optional;
 @Service
 public class ClientService {
 
-    @Autowired
-    ClientRepository repository;
+	@Autowired
+	ClientRepository repository;
 
-    public List<Client> getAllClient() throws NullPointerException {
-        List<Client> clientes = repository.findAll();
-        if (!clientes.isEmpty()) {
-            return clientes;
-        } else {
-            throw new NullPointerException("Valor nulo");
-        }
+	private static final Logger log4 = LoggerFactory.getLogger(ClientService.class);
 
-    }
+	/*
+	 * 
+	 * Devuelve una lista de todos los clientes que se encuentran en la BBDD
+	 * 
+	 * En caso de error nos devolveria un NullPointerException, ya que no hay nada,
+	 * la búsqueda sería nula
+	 * 
+	 */
+	public List<Client> getAllClient() throws NullPointerException {
+		List<Client> clientes = repository.findAll();
+		if (!clientes.isEmpty()) {
+			log4.info("Se han obtenido todos los clientes con éxito");
+			return clientes;
+		} else {
+			log4.error("ERROR: No se han encontrado clientes en la base de datos");
+			throw new NullPointerException("Valor nulo");
+		}
 
-    public Client getClientById(String id) throws RecordNotFoundException, NullPointerException, IllegalArgumentException {
-        if (id != null) {
-            try {
-                Optional<Client> client = repository.findById(id);
-                if (client.isPresent()) {
-                    return client.get();
-                } else {
-                    throw new RecordNotFoundException("No client record exist for given id", id);
-                }
-            } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException(e);
-            }
-        } else {
-            throw new NullPointerException("Valor nulo");
-        }
+	}
 
-    }
+	/*
+	 * 
+	 * Devuelve un cliente por un id de la BBDD
+	 * 
+	 * En caso de error nos devolveria un NullPointerException, ya que no hay nada,
+	 * la búsqueda sería nula
+	 * 
+	 */
+	public Client getClientById(String id)
+			throws RecordNotFoundException, NullPointerException, IllegalArgumentException {
+		if (id != null) {
+			try {
+				Optional<Client> client = repository.findById(id);
+				if (client.isPresent()) {
+					log4.info("Se ha obtenido el cliente por su ID con exito");
+					return client.get();
+				} else {
+					log4.error("No existe cliente con ese ID en la base de datos");
+					throw new RecordNotFoundException("No client record exist for given id", id);
+				}
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException(e);
+			}
+		} else {
+			log4.error("ERROR: No se ha podido traer el cliente por su ID, Valor nulo");
+			throw new NullPointerException("Valor nulo");
+		}
 
-    public Client createClient(Client cliente) {
-        cliente = repository.save(cliente);
-        return cliente;
-    }
+	}
 
-    public Client UpdateClient(Client entity) {
-        if (entity.getId() != null) {
-            Optional<Client> cliente = repository.findById(entity.getId());
+	/**
+	 * Guardamos un cliente en la base de datos
+	 * 
+	 * @param cliente Recibe un cliente que es el que guarda en la BBDD
+	 * @return devuelve el cliente que hemos añadido
+	 * 
+	 *         En caso de error nos devolveria un NullPointerException, ya que no
+	 *         hay nada, la búsqueda sería nula
+	 */
+	public Client createClient(Client cliente)
+			throws NullPointerException, IllegalArgumentException, RecordNotFoundException {
+		if (cliente != null) {
+			try {
+				cliente = repository.save(cliente);
+				log4.info("Creado el cliente Correctamente");
+				return cliente;
+			} catch (IllegalArgumentException e) {
+				log4.error("Se ha recibido valores incorrectos al crear el niño");
+				throw new IllegalArgumentException(e);
+			}
+		} else {
+			log4.error("ERROR: Hay datos nulos al crear el cliente");
+			throw new NullPointerException("Valor nulo");
+		}
 
-            if (cliente.isPresent()) {
-                Client newClient = cliente.get();
-                newClient.setEmail(entity.getEmail());
-                newClient.setName(entity.getName());
-                newClient.setPhone(entity.getPhone());
-                newClient.setSurname(entity.getSurname());
-                newClient.setType(entity.isType());
+	}
 
-                return newClient;
-            } else {
-                throw new RecordNotFoundException("Client not found", entity.getId());
-            }
-        } else {
-            throw new RecordNotFoundException("Client not found", entity.getId());
-        }
+	/**
+	 * Actualiza un cliente de la base de datos
+	 * 
+	 * @param entity Cliente que vamos a actualizar de la base de datos
+	 * @return Devuelve el nuevo cliente
+	 * 
+	 *         En caso de error nos devolveria un NullPointerException, ya que no
+	 *         hay nada, la búsqueda sería nula
+	 */
+	public Client UpdateClient(Client entity) {
+		if (entity.getId() != null) {
+			Optional<Client> cliente = repository.findById(entity.getId());
 
-    }
+			try {
+				if (cliente.isPresent()) {
+					Client newClient = cliente.get();
+					newClient.setEmail(entity.getEmail());
+					newClient.setName(entity.getName());
+					newClient.setPhone(entity.getPhone());
+					newClient.setSurname(entity.getSurname());
+					newClient.setType(entity.isType());
+					newClient = repository.save(newClient);
+					log4.info("Se ha actualizado el cliente correctamente");
+					return newClient;
+				} else {
+					log4.error("ERROR:Se han recibido datos incorrectos al actualizar cliente");
+					throw new IllegalArgumentException("Lo valores introducidos no son correctos ");
 
-    public void deleteClient(String id) throws RecordNotFoundException, NullPointerException, IllegalArgumentException {
-        if (id != null) {
-            try {
-                Optional<Client> cliente = repository.findById(id);
-                if (cliente.isPresent()) {
-                    repository.deleteById(id);
-                } else {
-                    throw new RecordNotFoundException("No client record exist for given id", id);
-                }
-            } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException(e);
-            }
+				}
 
-        } else {
-            throw new NullPointerException("Valor nulo");
-        }
+			} catch (Exception e) {
+				log4.error("ERROR:Valores mal introducidos en el cliente");
+				throw new RecordNotFoundException("Client not found", entity.getId());
+			}
 
-    }
+		} else {
+			log4.error("ERROR: Hay datos nulos al actualizar el cliemnte");
+			throw new RecordNotFoundException("Client not found", entity.getId());
+		}
+
+	}
+
+	/**
+	 * Borra de a base de datos por el id de un cliente
+	 * 
+	 * @param id Identificador de cliente
+	 * 
+	 *           En caso de error nos devolveria un NullPointerException, ya que no
+	 *           hay nada, la búsqueda sería nula
+	 * 
+	 */
+	public void deleteClient(String id) throws RecordNotFoundException, NullPointerException, IllegalArgumentException {
+		if (id != null) {
+			try {
+				Optional<Client> cliente = repository.findById(id);
+				if (cliente.isPresent()) {
+					log4.info("Eliminador el cliente correctamente");
+					repository.deleteById(id);
+				} else {
+					log4.error("ERROR:No existe cliente para eliminar con el ID");
+					throw new RecordNotFoundException("No client record exist for given id", id);
+				}
+			} catch (IllegalArgumentException e) {
+				log4.error("ERROR:Los datos introducidos para borrar el id no son correctos");
+				throw new IllegalArgumentException(e);
+			}
+
+		} else {
+			log4.error("ERROR:Los datos introducidos para borrar el id son nulos");
+			throw new NullPointerException("Valor nulo");
+		}
+
+	}
 }
