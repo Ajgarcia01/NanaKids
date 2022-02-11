1dbc4e0fb98f0b7d75f2ba8a51a49ed673d9b355
edu-cl123 <educar200@gmail.com>
2022-02-11 08:50:39 +0100

    Comentario y control Errores

--- src/main/java/com/app/ApiRestFul/controller/ClientController.java
+++ src/main/java/com/app/ApiRestFul/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.app.ApiRestFul.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.app.ApiRestFul.services.ClientService;
 import java.util.List;
@@ -22,87 +23,128 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/client")
 public class ClientController {
 
-    @Autowired
-    ClientService service;
-    //antes de tocar esto tener el servicio hecho
+	@Autowired
+	ClientService service;
+	// antes de tocar esto tener el servicio hecho
 
-    @GetMapping
-    public ResponseEntity<List<Client>> getAllClient() {
-        try {
-            List<Client> list = service.getAllClient();
-            return new ResponseEntity<List<Client>>(list, new HttpHeaders(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<List<Client>>(new HttpHeaders(), HttpStatus.NOT_FOUND);
-        } 
-    }
+	/**
+	 * @return EndPoint que nos devuelve un HttpStatus.OK y todos los clientes que
+	 *         existan en la BBDD a través del servicio
+	 */
+	@GetMapping
+	public ResponseEntity<List<Client>> getAllClient() {
+		try {
+			List<Client> list = service.getAllClient();
+			return new ResponseEntity<List<Client>>(list, new HttpHeaders(), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<List<Client>>(new HttpHeaders(), HttpStatus.NOT_FOUND);
+		}
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Client> getClientById(@PathVariable("id") String id)
-            throws RecordNotFoundException {
+	/**
+	 * @param id
+	 * @return cliente en concreto de la BBDD con el ID pasado por parámetro
+	 * @throws ResponseStatusException ( En caso de error nos devolveria
+	 *                                 unHttpStatus.NOT_FOUND o
+	 *                                 HttpStatus.BAD_REQUEST, en función de la
+	 *                                 petición)
+	 */
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> getClientById(@PathVariable("id") String id)
+			throws RecordNotFoundException, ResponseStatusException {
 
-        if (id != null) {
-            try {
-                Client admin = service.getClientById(id);
-                return new ResponseEntity<Client>(admin, new HttpHeaders(), HttpStatus.OK);
-            } catch (Exception e) {
-                return new ResponseEntity<Client>(new HttpHeaders(), HttpStatus.NOT_FOUND);
+		if (id != null) {
+			try {
+				Client admin = service.getClientById(id);
+				return new ResponseEntity<Client>(admin, new HttpHeaders(), HttpStatus.OK);
+			} catch (ResponseStatusException e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El cliente con id: " + id + "no se ha encontrado",
+						e);
+			}
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La peticion no se ha realizado correctamente");
+		}
 
-            }
-        } else {
-            return new ResponseEntity<Client>(new HttpHeaders(), HttpStatus.BAD_REQUEST);
-        }
+	}
 
-    }
+	/**
+	 * @param client
+	 * @return EndPoint que nos devuelve un HttpStatus.OK y un nuevo cliente creado
+	 *         en la BBDD
+	 * @throws ResponseStatusException ( En caso de error nos devolveria
+	 *                                 unHttpStatus.NOT_FOUND o
+	 *                                 HttpStatus.BAD_REQUEST, en función de la
+	 *                                 petición)
+	 */
+	@PostMapping
+	public ResponseEntity<Client> createClient(@RequestBody Client client)
+			throws RecordNotFoundException, ResponseStatusException {
+		if (client != null) {
+			try {
+				Client newClient = service.createClient(client);
+				return new ResponseEntity<Client>(newClient, new HttpHeaders(), HttpStatus.OK);
 
-    @PostMapping
-    public ResponseEntity<Client> createClient(@RequestBody Client client) {
-        if (client != null) {
-            try {
-                Client newClient = service.createClient(client);
-                return new ResponseEntity<Client>(newClient, new HttpHeaders(), HttpStatus.OK);
+			} catch (ResponseStatusException e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El cliente no ha sido guardado correctamente", e);
+			}
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La peticion no se ha realizado correctamente");
+		}
 
-            } catch (Exception e) {
-                return new ResponseEntity<Client>(new HttpHeaders(), HttpStatus.NOT_FOUND);
-            }
+	}
 
-        } else {
-            return new ResponseEntity<Client>(new HttpHeaders(), HttpStatus.BAD_REQUEST);
-        }
+	/**
+	 * @param client
+	 * @return EndPoint que nos devuelve un HttpStatus.OK y un cliente actualizado
+	 *         en la BBDD
+	 * @throws ResponseStatusException (nos devolveria unHttpStatus.NOT_FOUND o
+	 *                                 HttpStatus.BAD_REQUEST, en función de la
+	 *                                 petición)
+	 */
+	@PutMapping
+	public ResponseEntity<Client> UpdateClient(@RequestBody Client client)
+			throws RecordNotFoundException, ResponseStatusException {
+		if (client != null) {
+			try {
+				Client newClient = service.createClient(client);
+				return new ResponseEntity<Client>(newClient, new HttpHeaders(), HttpStatus.OK);
 
-    }
+			} catch (ResponseStatusException e) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El cliente no ha sido actualizado correctamente",
+						e);
+			}
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La peticion no se ha realizado correctamente");
+		}
 
-    @PutMapping
-    public ResponseEntity<Client> UpdateClient(@RequestBody Client client)
-            throws RecordNotFoundException {
-        if (client != null) {
-            try {
-                Client newClient = service.createClient(client);
-                return new ResponseEntity<Client>(newClient, new HttpHeaders(), HttpStatus.OK);
+	}
 
-            } catch (Exception e) {
-                return new ResponseEntity<Client>(new HttpHeaders(), HttpStatus.NOT_FOUND);
-            }
-        } else {
-            return new ResponseEntity<Client>(new HttpHeaders(), HttpStatus.BAD_REQUEST);
-        }
+	/**
+	 * @param id
+	 * @return EndPoint que nos devuelve un HttpStatus.OK y un cliente eliminado en
+	 *         la BBDD
+	 * @throws ResponseStatusException (En caso de error nos devolveria
+	 *                                 unHttpStatus.NOT_FOUND o
+	 *                                 HttpStatus.BAD_REQUEST, en función de la
+	 *                                 petición)
+	 */
+	@DeleteMapping("/{id}")
+	public HttpStatus deleteClientById(@PathVariable("id") String id)
+			throws RecordNotFoundException, ResponseStatusException {
 
-    }
+		if (id != null) {
+			try {
+				service.deleteClient(id);
+				return HttpStatus.ACCEPTED;
+			} catch (ResponseStatusException e) {
 
-    @DeleteMapping("/{id}")
-    public HttpStatus deleteClientById(@PathVariable("id") String id)
-            throws RecordNotFoundException {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El cliente no ha sido eliminado correctamente",
+						e);
+			}
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La peticion no se ha realizado correctamente");
+		}
 
-        if (id != null) {
-            try {
-                service.deleteClient(id);
-                return HttpStatus.ACCEPTED;
-            } catch (Exception e) {
-                return HttpStatus.NOT_FOUND;
-            }
-        } else {
-            return HttpStatus.BAD_REQUEST;
-        }
-
-    }
+	}
 
 }
