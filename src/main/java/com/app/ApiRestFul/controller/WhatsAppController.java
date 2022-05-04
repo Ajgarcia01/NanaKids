04e9f2b8cd0eecf6de3d634bb5e69fc98f84d21c
edu-cl123 <educar200@gmail.com>
2022-05-04 08:21:30 +0200

    Añadido

--- src/main/java/com/app/ApiRestFul/controller/WhatsAppController.java
+++ src/main/java/com/app/ApiRestFul/controller/WhatsAppController.java
@@ -1,23 +1,17 @@
 package com.app.ApiRestFul.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.app.ApiRestFul.model.Content;
-import com.app.ApiRestFul.model.Kid;
 import com.app.ApiRestFul.services.WhatsAppService;
 import com.twilio.rest.api.v2010.account.Message;
 
@@ -43,7 +37,6 @@ public class WhatsAppController {
       @ApiResponse(code = 400, message = "Bad request"),
       @ApiResponse(code = 500, message = "Internal Error ")
     })
-    @CrossOrigin(methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE})
 	@PostMapping
 	public ResponseEntity<Message> sendMessage(@RequestBody Content c) throws ResponseStatusException {
 		if (c!=null) {
--- src/main/java/com/app/ApiRestFul/services/WhatsAppService.java
+++ src/main/java/com/app/ApiRestFul/services/WhatsAppService.java
@@ -1,14 +1,12 @@
 package com.app.ApiRestFul.services;
 
 import java.net.URI;
-import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.app.ApiRestFul.exceptions.RecordNotFoundException;
-import com.app.ApiRestFul.model.Client;
 import com.app.ApiRestFul.model.Content;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
@@ -22,11 +20,12 @@ public class WhatsAppService {
 	private static final Logger log4 = LoggerFactory.getLogger(WhatsAppService.class);
 	
 	/**
-	 * @param kid
-	 * @return Crea un nino con los parametros pasados en la BBDD
-	 * @throws RecordNotFoundException,NullPointerException,IllegalArgumentException
+	 * 
+	 * @param c (Content)
+	 * @return message (URL de la imagen (Felicitacion), Cuerpo del Mensaje, Destinatario del mensaje (Client))
+	 * @throws NullPointerException
 	 */
-	public Message sendMessage(Content c) throws RecordNotFoundException {
+	public Message sendMessage(Content c) throws NullPointerException {
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 		Message message = null;
 		
@@ -37,7 +36,7 @@ public class WhatsAppService {
 		                c.getMessage()).setMediaUrl(URI.create(c.getFelicitation().getImage()))
 		            .create();
 			 log4.info("EL MENSAJE HA SIDO ENVIADO"+ message.getSid());
-		} catch (Exception e) {
+		} catch (NullPointerException e) {
 			log4.error("EL MENSAJE NO HA PODIDO SER ENVIADO "+e);
 			throw new RecordNotFoundException("ERROR EN EL ENVIO");
 		}
